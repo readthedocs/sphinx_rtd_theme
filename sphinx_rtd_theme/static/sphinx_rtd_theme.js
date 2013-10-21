5d5625ca0289e45620314242a88e5c964b307f0c
Dave Snider <dave.snider@gmail.com>
2013-10-21 15:52:35 -0700

    newest version

--- sphinx_rtd_theme/static/sphinx_rtd_theme.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready(function() {
-  $(".icon-reorder").click(function () {
-    $(".wy-nav-content-wrap").toggleClass("shift");
-    $(".wy-nav-side").toggleClass("shift");
-  });
-});
