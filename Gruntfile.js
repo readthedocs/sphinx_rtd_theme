97152e74c555efa7b4442239b00ea6fcf2e9e853
Dave Snider <dave.snider@gmail.com>
2013-11-03 13:37:56 -0800

    first pass breakpoint

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,45 +4,91 @@ module.exports = function(grunt) {
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   grunt.initConfig({
-    
-    compass: {
-      dev: {
+
+    connect: {
+      server: {
         options: {
-          config: 'sphinx_rtd_theme/sass/config.rb',
-          basePath: 'sphinx_rtd_theme/sass',
+          port: 9000,
+          base: 'demo_docs/build/html'
+        }
+      }
+    },
+
+    compass: {
+      src: {
+        options: {
+          config: 'src/sphinx_rtd_theme/sass/config.rb',
+          basePath: 'src/sphinx_rtd_theme/sass',
+          force: true
+        }
+      },
+      dist: {
+        options: {
+          config: 'src/sphinx_rtd_theme/sass/config.rb',
+          basePath: 'src/sphinx_rtd_theme/sass',
           outputStyle: 'compressed',
           force: true
         }
       }
     },
-    
+
     // I use this to build the sphinx_rtd_theme available at https://github.com/snide/sphinx_rtd_theme
     copy: {
-      sphinx_rtd_theme : {
+      dist : {
         files: [
           {
             expand: true,
-            cwd: 'docs/source/_themes/wyrm_theme', 
-            src: ['**', '!**/theme.sass', '!**/*.css', '!**/config.rb'],
-            dest: '../sphinx_rtd_theme/sphinx_rtd_theme'
+            cwd: 'src/sphinx_rtd_theme',
+            src: ['**', '!sass/'],
+            dest: 'dist/sphinx_rtd_theme'
           }
         ]
       }
     },
 
     exec: {
-      update: {
+      bower_update: {
         cmd: 'bower update'
+      },
+      build_sphinx: {
+        cmd: 'cd demo_docs && make html'
       }
+    },
+    clean: {
+      src: ["demo_docs/build"],
+      dist: ["dist/sphinx_rtd_theme"]
+    },
+
+    watch: {
+      sass: {
+        files: ['src/sphinx_rtd_theme/*.sass', 'bower_components/**/*.sass'],
+        tasks: ['compass:src']
+      },
+      /* watch and see if our javascript files change, or new packages are installed */
+      sphinx_update: {
+        files: ['src/sphinx_rtd_theme/static/*.css', 'src/sphinx_rtd_theme/*.js', 'demo_docs/source/*.rst', 'src/sphinx_theme/*.html'],
+        tasks: ['clean:src','exec:build_sphinx']
+      },
+      /* watch our files for change, reload */
+      livereload: {
+        files: ['demo_docs/build/html/**/*.html', 'demo_docs/build/html/_static/*.css', 'demo_docs/build/html/_static/*.js'],
+        options: {
+          livereload: true
+        }
+      },
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default', ['exec', 'compass']);
+  grunt.registerTask('default', ['bower_update','connect','watch']);
+  grunt.registerTask('build', ['clean:dist','compass:dist','copy:dist']);
 
 }
 
---
+++ demo_docs/source/index.rst
@@ -0,0 +1,22 @@
+.. Sphinx RTD theme demo documentation master file, created by
+   sphinx-quickstart on Sun Nov  3 11:56:36 2013.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Sphinx RTD theme demo's documentation!
+=================================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
