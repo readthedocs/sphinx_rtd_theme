f199e76f653426f43d3bd0c18d403408494a04eb
Tony Narlock <tony@git-pull.com>
2013-11-03 11:26:36 +0800

    Python package

---
+++ setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""`sphinxcontrib-rtd_theme` lives on `Github`_.
+
+.. _github: https://www.github.com/snide/sphinx_rtd_theme
+
+"""
+from setuptools import setup, find_packages
+from sphinx_rtd_theme import __version__
+
+try:
+    from pip.req import parse_requirements
+except ImportError:
+    def requirements(f):
+        reqs = open(f, 'r').read().splitlines()
+        reqs = [r for r in reqs if not r.strip().startswith('#')]
+        return reqs
+else:
+    def requirements(f):
+        install_reqs = parse_requirements(f)
+        reqs = [str(r.req) for r in install_reqs]
+        return reqs
+
+
+setup(
+    name='sphinxcontrib_rtd_theme',
+    version=__version__,
+    url='https://github.com/snide/sphinx_rtd_theme/',
+    license='MIT',
+    author='Dave Snider',
+    author_email='dave.snider@gmail.com',
+    description='ReadTheDocs.org theme for Sphinx, 2013 version.',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    packages=find_packages(),
+    package_data = { "sphinx_rtd_theme": ['*.*', 'static/*.*', 'static/*/*.*'] },
+    install_requires=['sphinx>=1.1'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ],
+)
