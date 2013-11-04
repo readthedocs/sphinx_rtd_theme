a8ea26153b60334d67f453b064690cb6cced2594
Matthew Iversen <teh.ivo@gmail.com>
2013-11-04 17:27:53 +1100

    rename tasks, use sphinx-build, add comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
       server: {
         options: {
           port: 1919,
-          base: 'demo_docs/build/html',
+          base: 'demo_docs/build',
           livereload: true
         }
       }
@@ -41,26 +41,27 @@ module.exports = function(grunt) {
         cmd: 'bower update'
       },
       build_sphinx: {
-        cmd: 'cd demo_docs && make html'
+        cmd: 'sphinx-build demo_docs/source demo_docs/build'
       }
     },
     clean: {
-      src: ["demo_docs/build"]
+      build: ["demo_docs/build"]
     },
 
     watch: {
+      /* Compile sass changes into theme directory */
       sass: {
         files: ['sass/*.sass', 'bower_components/**/*.sass'],
-        tasks: ['compass:rtd']
+        tasks: ['compass:debug']
       },
-      /* watch and see if our javascript files change, or new packages are installed */
+      /* Changes in theme dir rebuild sphinx */
       sphinx: {
         files: ['sphinx_rtd_theme/**/*'],
         tasks: ['exec:build_sphinx']
       },
-      /* watch our files for change, reload */
+      /* live-reload the demo_docs if sphinx re-builds */
       livereload: {
-        files: ['demo_docs/**/*'],
+        files: ['demo_docs/build/**/*'],
         options: { livereload: true }
       }
     }
@@ -74,7 +75,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-open');
 
-  grunt.registerTask('default', ['exec:bower_update','clean:src','compass:debug','exec:build_sphinx','connect','open','watch']);
-  grunt.registerTask('build', ['exec:bower_update','clean:src','compass:prod','exec:build_sphinx']);
+  grunt.registerTask('default', ['exec:bower_update','clean:build','compass:debug','exec:build_sphinx','connect','open','watch']);
+  grunt.registerTask('build', ['exec:bower_update','clean:build','compass:prod','exec:build_sphinx']);
 }
 
