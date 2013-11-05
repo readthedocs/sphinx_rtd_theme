20d43a68bbe14d29c7246dde185500ec7cfee001
Tony Narlock <tony@git-pull.com>
2013-11-05 08:37:01 +0800

    Add new tests for sphinx.ext.autodoc, and sphinx.ext.autodoc embedded reST content. Remove code mixin from tt and address the usage of tt for descclass, descclassname. and the normal tt, which is for inline markup. Clears ambiguity on css rules. Updates the test_py_module with new content, pep257.

--- demo_docs/source/test_py_module/test.py
+++ demo_docs/source/test_py_module/test.py
@@ -1,7 +1,31 @@
 # -*- coding: utf-8 -*-
+"""Test Module for sphinx_rtd_theme."""
 
 class Foo:
-    """Docstring for class Foo."""
+
+    r"""Docstring for class Foo.
+
+    This text tests for the formatting of docstrings generated from output
+    ``sphinx.ext.autodoc``. Which contain reST, but sphinx nests it in the
+    ``<dl>``, and ``<dt>`` tags. Also, ``<tt>`` is used for class, method names
+    and etc, but those will *always* have the ``.descname`` or
+    ``.descclassname`` class.
+
+    Normal ``<tt>`` (like the <tt> I just wrote here) needs to be shown with
+    the same style as anything else with ````this type of markup````.
+
+    It's common for programmers to give a code example inside of their
+    docstring::
+
+        from test_py_module import Foo
+
+        myclass = Foo()
+        myclass.dothismethod('with this argument')
+        myclass.flush()
+
+        print(myclass)
+
+    """
 
     #: Doc comment for class attribute Foo.bar.
     #: It can have multiple lines.
