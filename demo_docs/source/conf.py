8bd1938c8b9ad66445608b42c3bac287185bbfc2
Daniel Oaks <danneh@danneh.net>
2013-12-08 23:18:43 +1000

    html logo_only is now an option

--- demo_docs/source/conf.py
+++ demo_docs/source/conf.py
@@ -106,7 +106,9 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    # 'logo_only': True,  # if we have a html_logo below, this shows /only/ the logo with no title text
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ["../.."]
