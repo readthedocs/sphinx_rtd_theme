bcf2276d5b5a41144c23e818ca5f203c032fb251
Eric Hough <eric@tubepress.org>
2014-01-05 17:17:41 -0500

    Adding sticky navigation option

--- demo_docs/source/conf.py
+++ demo_docs/source/conf.py
@@ -106,7 +106,9 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'sticky_navigation' : False  # Set to True to "stick" the navigation during scrolling
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ["../.."]
