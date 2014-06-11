3f94da0584e50470409000cfdf0f588d9304d1d7
Will Stevens <wstevens@cloudops.com>
2014-06-11 13:22:23 -0400

    added an example to the demo docs and configured it to use sticky nav for the demo to work

--- demo_docs/source/conf.py
+++ demo_docs/source/conf.py
@@ -107,7 +107,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    # 'sticky_navigation' : True  # Set to False to disable the sticky nav while scrolling.
+    'sticky_navigation' : True  # Set to False to disable the sticky nav while scrolling.
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
