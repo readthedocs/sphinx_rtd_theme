7fabadeb40e884478f013afc8e61268ef8751906
Aaron Carlisle <carlisle.b3d@gmail.com>
2017-03-15 00:27:29 -0400

    Fix own error

--- demo_docs/source/conf.py
+++ demo_docs/source/conf.py
@@ -14,6 +14,7 @@
 import sys
 import os
 
+sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('./test_py_module'))
 
 # If extensions (or modules to document with autodoc) are in another directory,
