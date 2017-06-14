1056aea4ab172f254018f5da6459b4be334592c1
Segev Finer <segev208@gmail.com>
2017-06-14 08:07:35 +0300

    Added a link to the Sphinx documentation about setuptools entry points

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'static/font/*.*'
     ]},
     include_package_data=True,
+    # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
     entry_points = {
         'sphinx.html_themes': [
             'sphinx_rtd_theme = sphinx_rtd_theme',
