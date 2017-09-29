dd3b72618f3a3d50a7be94453ffaa0f5552f7805
Aaron Carlisle <carlisle.aaron00@gmail.com>
2017-09-28 21:52:59 -0400

    Address Review: Remove uses of os.path

--- sphinx_rtd_theme/__init__.py
+++ sphinx_rtd_theme/__init__.py
@@ -11,7 +11,7 @@ __version_full__ = __version__
 
 def get_html_theme_path():
     """Return list of HTML theme paths."""
-    cur_dir = path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    cur_dir = path.abspath(path.dirname(path.dirname(__file__)))
     return cur_dir
 
 # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
