d8d8fa5fa0f7081371d2a97af5d76a352f0fd524
Jesse Tan <j.tan@activevideo.com>
2017-11-24 12:02:56 +0100

    Document workaround for footnote and citation spacing

--- js/theme.js
+++ js/theme.js
@@ -77,9 +77,10 @@ function ThemeNav () {
         $("table.docutils:not(.field-list,.footnote,.citation)")
             .wrap("<div class='wy-table-responsive'></div>");
 
+        // Add extra class to responsive tables that contain
+        // footnotes or citations so that we can target them for styling
         $("table.docutils.footnote")
             .wrap("<div class='wy-table-responsive footnote'></div>");
-
         $("table.docutils.citation")
             .wrap("<div class='wy-table-responsive citation'></div>");
 
