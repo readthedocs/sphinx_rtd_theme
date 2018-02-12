8fb6a0ffcc24e0fa4325888dd721ed3590f8f67a
Aaron Carlisle <carlisle.b3d@gmail.com>
2018-02-12 18:45:25 -0500

    Replace the name 'Dave Snider' with RTD

--- docs/conf.py
+++ docs/conf.py
@@ -204,7 +204,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'SphinxRTDthemedemo.tex', u'Sphinx RTD theme demo Documentation',
-   u'Dave Snider', 'manual'),
+   u'Read the Docs team', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -234,7 +234,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'sphinxrtdthemedemo', u'Sphinx RTD theme demo Documentation',
-     [u'Dave Snider'], 1)
+     [u'Read the Docs team'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -248,7 +248,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'SphinxRTDthemedemo', u'Sphinx RTD theme demo Documentation',
-   u'Dave Snider', 'SphinxRTDthemedemo', 'One line description of project.',
+   u'Read the Docs team', 'SphinxRTDthemedemo', 'One line description of project.',
    'Miscellaneous'),
 ]
 
