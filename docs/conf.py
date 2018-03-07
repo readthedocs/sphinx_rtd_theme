f3cb3c5688aa2b82b95ac6129367132be99e9005
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-03-07 10:22:41 -0500

    Give credit back to Dave + RTD

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Read the Docs Sphinx Theme'
-copyright = u'Read the Docs, Inc & contributors'
+copyright = u'Dave Snider, Read the Docs, Inc. & contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -204,7 +204,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'SphinxRTDthemedemo.tex', u'Sphinx RTD theme demo Documentation',
-   u'Read the Docs, Inc & contributors', 'manual'),
+   u'Dave Snider, Read the Docs, Inc. & contributors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -234,7 +234,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'sphinxrtdthemedemo', u'Sphinx RTD theme demo Documentation',
-     [u'Read the Docs, Inc & contributors'], 1)
+     [u'Dave Snider, Read the Docs, Inc. & contributors'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -248,7 +248,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'SphinxRTDthemedemo', u'Sphinx RTD theme demo Documentation',
-   u'Read the Docs, Inc & contributors', 'SphinxRTDthemedemo',
+   u'Dave Snider, Read the Docs, Inc. & contributors', 'SphinxRTDthemedemo',
    'One line description of project.', 'Miscellaneous'),
 ]
 
