ad90d0d3c306252aadcf5b7b7cf308321c0540ad
Ashley Whetter <asw@dneg.com>
2019-07-25 12:56:32 -0700

    Improved navigation of httpdomain endpoints

--- js/theme.js
+++ js/theme.js
@@ -239,3 +239,41 @@ if (typeof(window) != 'undefined') {
             clearTimeout(id);
         };
 }());
+
+
+// sphinxcontrib-httpdomain extensions
+
+$(document).ready(function() {
+    var collapse_dd = function () {
+        if ($(this).parent().hasClass('collapsed')) {
+            $(this).parent().removeClass('collapsed');
+            $(this).parent().children('dd').slideDown('fast');
+        } else {
+            $(this).parent().addClass('collapsed');
+            $(this).parent().children('dd').slideUp('fast');
+        }
+        return false;
+    };
+
+    var ops = ['post', 'get', 'put', 'patch', 'delete'];
+    ops.forEach(function(op) {
+        $('dl.' + op + ' > dd').hide();
+        $('dl.' + op).addClass('collapsed');
+        $('dl.' + op + ' > dt').click(collapse_dd);
+        $('dl.' + op + ' > dt > a').click(function(e) { e.stopPropagation(); });
+        $('dl.' + op + ' > dd > p:first-child').each(function() {
+            $(this).parent().parent().children('dt').first().append($(this))
+        });
+    });
+
+    $(window).on('hashchange', function() {
+        if (window.location.hash.length != 0) {
+            base = $(window.location.hash.replace(/[\(\)]/g, '\\$&'));
+            section = base.closest('.collapsed');
+            section.removeClass('collapsed');
+            section.children('dd').show();
+            $(document).scrollTop(base.offset().top);
+        }
+    });
+});
+
