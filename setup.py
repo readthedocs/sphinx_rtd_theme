af521d644dced902112294e9e53e71320c51b20f
Anthony Johnson <aj@ohess.org>
2019-07-29 11:04:41 -0600

    Make super() py2/3 compat

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class WebpackBuildCommand(setuptools.command.build_py.build_py):
     def run(self):
         if not 'CI' in os.environ:
             subprocess.run(['webpack', '--config', 'webpack.prod.js'], check=True)
-        super().run()
+        super(WebpackBuildCommand, self).run()
 
 
 class WebpackDevelopCommand(distutils.cmd.Command):
