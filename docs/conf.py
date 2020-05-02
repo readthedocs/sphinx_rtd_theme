16d874d984e8cb5cc5b0c31ced31ea76cf063bcf
Anthony Johnson <aj@ohess.org>
2020-05-02 16:47:06 -0600

    Use the local theme for PR builds on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,20 @@ import sys
 import os
 import re
 
-if not 'READTHEDOCS' in os.environ:
+# If we are building locally, or the build on Read the Docs looks like a PR
+# build, prefer to use the version of the theme in this repo, not the installed
+# version of the theme.
+def is_development_build():
+    # PR builds have an interger version
+    re_version = re.compile(r'^[\d]+$')
+    if 'READTHEDOCS' in os.environ:
+        version = os.environ.get('READTHEDOCS_VERSION', '')
+        if re_version.match(version):
+            return True
+        return False
+    return True
+
+if is_development_build():
     sys.path.insert(0, os.path.abspath('..'))
 sys.path.append(os.path.abspath('./demo/'))
 
