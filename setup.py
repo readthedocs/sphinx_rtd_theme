50c7fefbe50ccde29d434d849e8b4c77a370e44a
Aaron Carlisle <carlisle.b3d@gmail.com>
2021-03-15 18:09:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -138,7 +138,6 @@ setup(
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
