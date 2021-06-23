4147e0fcff82b8044eff9dcee3d39a3cafc0fe0d
Joe Nelson <jonels@microsoft.com>
2021-06-23 12:16:18 -0500

    Generate static css/js, bump version

--- sphinx_rtd_theme_citus/__init__.py
+++ sphinx_rtd_theme_citus/__init__.py
@@ -12,7 +12,7 @@ from sphinx.locale import _
 from sphinx.util.logging import getLogger
 
 
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 __version_full__ = __version__
 
 logger = getLogger(__name__)
