dc753f1667ba558c4119b9c87bc180bf4e68c72f
Phil Elson <philip.elson@cern.ch>
2022-09-28 09:07:42 +0200

    No reason to only have the first three elements from the sphinx version

--- sphinx_rtd_theme/__init__.py
+++ sphinx_rtd_theme/__init__.py
@@ -34,7 +34,7 @@ def config_initiated(app, config):
 
 def extend_html_context(app, pagename, templatename, context, doctree):
      # Add ``sphinx_version_info`` tuple for use in Jinja templates
-     context['sphinx_version_info'] = sphinx_version[:3]
+     context['sphinx_version_info'] = sphinx_version
 
 
 # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
