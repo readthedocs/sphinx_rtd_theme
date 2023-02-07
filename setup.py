fc26a4a962b144ead35478c0a71086d3fa4dbe46
Anthony Johnson <aj@ohess.org>
2023-02-07 13:51:36 -0800

    Bump to next alpha for development

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ class TransifexCommand(distutils.cmd.Command):
 
 
 setup(
-    version='1.2.0',
+    version='1.2.1alpha1',
     cmdclass={
         'update_translations': UpdateTranslationsCommand,
         'transifex': TransifexCommand,
