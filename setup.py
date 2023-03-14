f2fcc88e5c43b7061d229932f304f58e205cbd48
Steven Klavins <stevenklavins94@gmail.com>
2023-03-14 15:08:55 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup(
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     install_requires=[
         'sphinx>=1.6',
-        'docutils<0.19',
+        'docutils>=0.14,<0.18',
     ],
     tests_require=[
         'pytest',
