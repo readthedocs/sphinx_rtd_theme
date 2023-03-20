305327835038db5c83b28043f01e0c44f224a471
Benjamin Bach <benjamin@readthedocs.org>
2023-03-20 16:06:31 +0100

    Avoid importing from sphinxcontrib.jquery in __init__

--- sphinx_rtd_theme/__init__.py
+++ sphinx_rtd_theme/__init__.py
@@ -10,7 +10,6 @@ from sys import version_info as python_version
 from sphinx import version_info as sphinx_version
 from sphinx.locale import _
 from sphinx.util.logging import getLogger
-from sphinxcontrib.jquery import add_js_files as jquery_add_js_files
 
 
 __version__ = '1.2.1alpha1'
@@ -60,6 +59,7 @@ def setup(app):
         # See: https://www.sphinx-doc.org/en/master/extdev/appapi.html#sphinx.application.Sphinx.setup_extension
         app.setup_extension("sphinxcontrib.jquery")
         # However, we need to call the extension's callback since setup_extension doesn't do it
+        from sphinxcontrib.jquery import add_js_files as jquery_add_js_files
         jquery_add_js_files(app, app.config)
 
     # Register the theme that can be referenced without adding a theme path
