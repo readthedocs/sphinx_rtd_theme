49f6b7b8d8b7989c030f28d8c161a7d1d6aef126
Anthony Johnson <aj@ohess.org>
2023-08-21 11:51:21 -0700

    Release 2.0.0rc1

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ class TransifexCommand(distutils.cmd.Command):
 
 
 setup(
-    version='1.3.0',
+    version='2.0.0rc1',
     cmdclass={
         'update_translations': UpdateTranslationsCommand,
         'transifex': TransifexCommand,
