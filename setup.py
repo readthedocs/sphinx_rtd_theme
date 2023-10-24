1454eee461b387b748b15d99833d2b0fd7d05be4
Anthony Johnson <aj@ohess.org>
2023-10-23 19:16:17 -0700

    Increment for next potential release

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ class TransifexCommand(distutils.cmd.Command):
 
 
 setup(
-    version='2.0.0rc3',
+    version='2.0.0rc4',
     cmdclass={
         'update_translations': UpdateTranslationsCommand,
         'transifex': TransifexCommand,
