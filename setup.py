b1ec073c0123ebeb9d4b3f59b4c31509beae2eaf
Manuel Kaufmann <humitos@gmail.com>
2024-07-22 13:37:06 +0200

    Release 2.1.0rc1
    I wasn't able to update translations because it fails with non-zero exit when
    running `python setup.py update_translations`.

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ class TransifexCommand(distutils.cmd.Command):
 
 
 setup(
-    version='2.0.1alpha1',
+    version='2.1.0rc1',
     cmdclass={
         'update_translations': UpdateTranslationsCommand,
         'transifex': TransifexCommand,
